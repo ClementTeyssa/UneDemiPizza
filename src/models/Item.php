23a421134f1505e10c0ba9a4cba499f2d120c879
Clement <clement.teyssandier9@etu.univ-lorraine.fr>
2018-02-08 12:30:51 +0100

    Modif des models

--- src/models/Item.php
+++ src/models/Item.php
@@ -1,21 +1,23 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Clement
- * Date: 08/02/2018
- * Time: 11:20
+ * User: Clément Teyssandier
+ * Date: 22/01/2018
+ * Time: 22:26
  */
 
 namespace pizza\models;
 
+
 class Item extends \Illuminate\Database\Eloquent\Model
 {
+
     protected $table = 'item';
     protected $primaryKey = 'id';
     public $timestamps = false;
 
     public static function getById($id){
-        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+        $id = filter_var($id, FILTER_SANITIZE_STRING);
         return Item::where('id', '=', $id)->first();
     }
 }
