bec732dca38881626d37b7caf4ef447cd4c824ac
Clement <clement.teyssandier9@etu.univ-lorraine.fr>
2018-02-08 17:28:32 +0100

    Ajout fonction pour calendrier

--- src/vues/VueItem.php
+++ src/vues/VueItem.php
@@ -31,17 +31,55 @@ class VueItem{
 		$nom = $this->objet->nom;
 		$id = $this->objet->id;
 		$desc = $this->objet->description;
-		
-		$content = <<<end
-		<h1>Item $nom </h1>
-		<br>
-		<p>Id : $id.</p>
-		<br>
-		<p>$desc</p>
-		<br>
-		
-end;
+        $mois = date('m');
+        $annee = date('Y');
 
+        $content = <<<end
+		<h1>Item $nom </h1>
+		<div class="row">
+		    <div class="col s6">
+                <p>Id : $id.</p>
+                <br>
+                <p>$desc</p>
+            </div>
+            <div class="col s1">
+                <i class="material-icons" id="prev">arrow_back</i>
+            </div>
+            <div class="col s4">
+end;
+	     $content .=
+             "<script>
+             $(function($){
+	                    var mois = $mois;
+	                    var annee = $annee;
+	                    $(document).ready(function(){
+	                        $(\"#content\").load(\"calendrier.php?mois=\"+mois+\"&annee=\"+annee+\"\");
+	                    });
+	                    $(\"#prev\").click(function(){
+	                        mois--;
+                            if (mois < 1) {
+                                annee--;
+                                mois = 12;
+                            }
+                            $(\"#content\").load(\"calendrier.php?mois=\"+mois+\"&annee=\"+annee+\"\");
+                        });
+	                    $(\"#next\").click(function(){
+                            mois++;
+                            if (mois > 12) {
+                                annee++;
+                                mois = 1;
+                            }
+		                    $(\"#content\").load(\"calendrier.php?mois=\"+mois+\"&annee=\"+annee+\"\"); 
+	                    });
+
+                });";
+	     $content .= "
+	            </script>
+            </div>
+            <div class='col s1'>
+                <i class='material-icons' id='prev'>arrow_forward</i>
+            </div>
+		</div>";
 		return $content;
 	}
 }
